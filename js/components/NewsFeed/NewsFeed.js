193af5b497fbc94775ab1611e5ffb8798338b4d1
Olmeiro Orozco <olmeiro.orozco@gmail.com>
2020-05-28 20:20:19 -0500

    first asesory

--- js/components/NewsFeed/NewsFeed.js
+++ js/components/NewsFeed/NewsFeed.js
@@ -9,73 +9,43 @@ var NewsFeed = (function () {
 
     // Create an ajax get request to obtain data
     $.ajax({
-      url: "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines",
+      url: "https://cors-anywhere.herokuapp.com/https://newsapi.org/v2/top-headlines?country=co&pageSize=4&apikey=8e63316767f14a0bba6e4fbca94276e2",
       type: "GET",
       data: utils.toQueryString(params),
       success(data, text) {
-        console.log('data', data);
+        
+        var articles = data.articles;
+        console.log(articles);
+       
+        var $newFeed = $('.news-feed');
+        $newFeed.css("background-color", "aquamarine");
 
-        // $("#demo").html(data);
-
-        // x = data.articles[0].author;
-        // y = data.articles[1].content;
-        // console.log(y)i
-        for (i= 0; i <= 3; i++)
-        {
-          let titulo = data.articles[i].title;
-          let descripcion = data.articles[i].description;
-          let imagen = data.articles[i].urlToImage;
-          let fecha = data.articles[i].publishedAt;
-
-          console.log(imagen);
-
+        articles.forEach(function(article){
+          var titleNew = $('<span>',{html:article.title});
+          var authorArticle = $('<span>',{html:article.author});
+          var descriptionArticle = $('<span>', {html:article.description});
+          var dateNew = $('<span>', {html:article.publishedAt});
           
-         document.getElementById("titulo").innerHTML += titulo + "<br>";
-         document.getElementById("descripcion").innerHTML += descripcion + "<br>";
-        document.getElementById("imagen").innerHTML += imagen + "<br>";
-        document.getElementById("fecha").innerHTML += fecha + "<br>";
+          // var imgUrl = $('<img>', {html:article.urlToImage});
+          // var image = $('<img>').attr(src);
 
-        }
+          //   console.log(imgUrl);
 
+          var test = titleNew.html() + "<br><br>" + authorArticle.html() + '<br><br>' + descriptionArticle.html() + "<br><br>" + dateNew.html() ;
+          // + "<br>" + image.html()
+          console.log(test);
+          var container = $('<div>', {html:(test)});
 
-     
-        // let titulo = data.articles[19].title;
-        // let descripcion = data.articles[19].description;
-        // let imagen = data.articles[19].urlToImage;
-        // let fecha = data.articles[19].publishedAt;
+          $newFeed.append(container);
 
-        // var x = "";
-        // var y = "";
-        // var c = "";
-
-        //  document.getElementById("titulo").innerHTML += titulo;
-        //  document.getElementById("descripcion").innerHTML += descripcion;
-        // document.getElementById("imagen").innerHTML += imagen;
-        // document.getElementById("fecha").innerHTML += fecha;
-
-        // for (i in data.articles) {
-        //   // x +=  data.articles[i].title;
-        //   x += "<h2>" + data.articles[i].title + "</h2>";
-         
-        //   for (j in data.articles[i].author) {
-        //     x += data.articles[i].author[j] ;
-            
-        //   }
-        //   for(k in data.articles[i].content){
-        //     x += data.articles[i].content[k] ;
-           
-        //   }
-          
-        // }
-
-        // document.getElementById("title").innerHTML = x;
-          // document.getElementById("autor").innerHTML = y;
-          // document.getElementById("contenido").innerHTML = c;
+        });
 
       },
       error(request, status, error) {
         console.log('error', error)
       }
+
+   
     });
   }
 
